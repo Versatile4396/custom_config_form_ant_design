535445d549c2a1d47d57e02a54cedd6e1bb21216
huangmande <huangmande@bytedance.com>
2024-09-09 13:50:03 +0800

    feat: 初始化架构

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="main-form">
-    <Form></Form>
+    <div class="form-wrapper">
+      <Form></Form>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
 import Form from "./components/configForm/index.vue";
 </script>
-<style scoped>
+<style scoped lang="scss">
 .main-form {
+  padding-top: 20px;
   display: flex;
   justify-content: center;
+  .form-wrapper {
+    width: 40vw;
+    min-height: 100px;
+    background: radial-gradient(
+      100% 157.26% at 0% 0%,
+      rgba(217, 242, 255, 0.5) 0%,
+      rgba(242, 240, 252, 0.5) 27.87%,
+      rgba(191, 216, 255, 0.5) 100%
+    );
+    border-radius: 16px;
+  }
 }
 </style>
--- src/components/configForm/index.vue
+++ src/components/configForm/index.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="">asd</div>
+  <div class="form-wrapper">
+    <a-button>asd</a-button>
+    <a-button>asd</a-button>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { Button } from "ant-design-vue";
+</script>
 <style scoped lang="scss">
 /* @import url(); 引入css类 */
+.form-wrapper {
+  padding: 20px;
+}
 </style>
